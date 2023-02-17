7611b77d1d25025a045542398396087fc71c1db3
Sviatoslav Kostyshyn <kossvat1999@gmail.com>
2023-02-16 20:37:07 -0500

    Added allowed hosts

--- deploy_v2/setup.sh
+++ deploy_v2/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/kossvat/profile-rest-api-2.git'
 
-PROJECT_BASE_PATH='/usr/local/apps'
+PROJECT_BASE_PATH='/usr/local/apps/profile-rest-api-2'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 
 # Set Ubuntu Language
